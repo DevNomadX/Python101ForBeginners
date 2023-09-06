fe2e53ff590b6d7603e45e63b24f06394d576876
crazycoder112 <hr1205909@gmail.com>
2023-09-06 20:52:46 +0600

    adding 2 problem solving python program problem:1 and problem:2

---
+++ problem-1 add tow number.py
@@ -0,0 +1,22 @@
+"""
+Problem 1 : define sum of tow number
+
+"""
+a = 10
+b = 22
+sum = a + b
+
+print("The sum of a and b is :", sum)
+
+
+# best practice
+"""def add_numbers():
+    a = 10
+    b = 22
+    sum = a + b
+
+    return sum
+
+
+if __name__ == "__main__":
+    print("the sum of a and b is: ", add_numbers())"""
---
+++ problem-2 find the area of rectangle.py
@@ -0,0 +1,13 @@
+"""
+Problem 2 : define the area of a rectangle
+
+"""
+# for geting input from user we need to use input() function
+# and int() is used to take the input as integer
+
+length = int(input("Enter value of Length: "))
+width = int(input("Enter width: "))
+
+RectangleArea = length * width
+
+print(RectangleArea)
