0c0fe9aba84d6ac6a2cf1bccc199577ee38445bb
crazycoder112 <hr1205909@gmail.com>
2023-09-06 21:25:37 +0600

    adding problem 4 ans.

---
+++ problem-4 defind the area of circle.py
@@ -0,0 +1,14 @@
+"""
+Problem 4 : find the area of Circle
+
+"""
+
+import math
+
+# define pi as a constant value
+PI = math.pi
+r = 5
+
+CircleArea = PI * (r**2)
+
+print("The area of circle is : ", CircleArea)
