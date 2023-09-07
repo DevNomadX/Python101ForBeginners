eaf21e89e5fde719909722c5d5a50c90c99050b3
crazycoder112 <hr1205909@gmail.com>
2023-09-07 21:37:56 +0600

    Adding problem number 6 ans.

---
+++ problem-6 define largest number from three numbers.py
@@ -0,0 +1,15 @@
+a = 1000
+b = 200
+c = 30
+
+# Showing input value using "F" string
+print(f"A = {a}, B = {b}, C = {c}")
+
+if a > b and a > c:
+    print(" A is large")
+
+elif b > a and b > c:
+    print(" B is large")
+
+else:
+    print("C is large")
