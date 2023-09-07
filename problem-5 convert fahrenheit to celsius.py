4bb486b9526a5be4b5cea5d9bc8e9edd00e56cae
crazycoder112 <hr1205909@gmail.com>
2023-09-07 21:22:51 +0600

    Adding problem number 5 ans.

---
+++ problem-5 convert fahrenheit to celsius.py
@@ -0,0 +1,22 @@
+"""
+Problem 5: Get Fahrenheit Temp from Keybord And Convert into Celsius
+
+"""
+
+f = float(input("Enter Fahrenheit Value: "))
+celsius = (f - 32) * 5 / 9
+
+print(celsius)
+
+
+# Best Practice
+"""def convert(f):
+    celsius = (f - 32) * 5 / 9
+
+    return celsius
+
+
+f = float(input("Enter Fahrenheit Value: "))
+
+if __name__ == "__main__":
+    print(convert(f), "Celsius")"""
